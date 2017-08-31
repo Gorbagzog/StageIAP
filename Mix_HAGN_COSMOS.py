dcff8ab343e3879dead5fbe7c5d3207dc3d9c959
louis legrand <llegrand@ias.u-psud.fr>
2017-08-31 19:33:16 +0200

    mix HAGN photo and cosmos

--- Mix_HAGN_COSMOS.py
+++ Mix_HAGN_COSMOS.py
@@ -1,6 +1,6 @@
 """Scrit to plot Horizon AGN and COSMOS on the same plot.
 
-First need to load the data form AM_COSMOSIari_BolsoiPlanck.py and HorizonAGNE.py
+First need to load the data form AM_COSMOSIari_BolsoiPlanck.py and HorizonAGN.py
 """
 
 import numpy as np
@@ -66,3 +66,35 @@ for i in range(3):
     plt.ylabel('Log($M_{s}/M_{h}$)', size=15)
 plt.tight_layout()
 plt.show()
+
+
+"""Plot Ms_observed(Mh) and level 1 halos"""
+
+for i in range(3):
+    plt.figure()
+    indices = np.where(
+        np.logical_and(
+            np.logical_and(hal_centgal[i] > 0, halodata[i]['level'] >= 1),
+            galdata_allz['Obs_gal_idx'][
+                    hal_centgal[i][:] - 1 + sum(len(galdata[j]) for j in range(i))
+            ] > 0
+        )
+    )
+    plt.hist2d(
+        np.log10(halodata[i]['Mass'][indices]*10**11),
+        galphot['Mass'][
+                galdata_allz['Obs_gal_idx'][
+                    hal_centgal[i][indices] - 1 + sum(len(galdata[j]) for j in range(i))
+                ].astype('int')
+        ],
+        bins=100, cmin=1, range=[[10, 14], [9, 12]])
+    plt.colorbar()
+    for j in select[i]:
+        plt.plot(xm[j][:], MstarIary[j](x[j]), label=str(redshifts[j]) + '<z<' + str(redshifts[j + 1]))
+        plt.fill_between(xm[j], MstarIaryMinus[j](x[j]), MstarIaryPlus[j](x[j]), alpha=0.7)
+    plt.legend()
+    plt.xlabel('Log($M_{h}$) [Log($M_{\odot}$)]', size=12)
+    plt.ylabel('Log($M_{*}$) Photometric [Log($M_{\odot}$)]', size=12)
+    plt.title('HorizonAGN, z='+str(zbins_Cone[i])+'-'+str(zbins_Cone[i+1]))
+    # plt.savefig('../Plots/HAGN_Matching/ClotMatch/PhotoMass_HaloMass' +
+    #             str(zbins_Cone[i])+'-'+str(zbins_Cone[i+1]) + '.pdf')
