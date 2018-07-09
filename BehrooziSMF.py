4d0a9598a6d3cf6e1e0b7effb92dfe6ed29bf5a9
llegrand <biglouis06@hotmail.fr>
2018-07-09 17:56:44 +0200

    starts behroozi SMF computation.

---
+++ BehrooziSMF.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*-coding:Utf-8 -*
+
+"""Script to compute the SMF from Behroozi et al. 2013 and Behroozi et al. 2018"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from MCMC_SHMR_main.py import *
+
+
