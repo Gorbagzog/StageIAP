a8fddf58115d85a9c761204d4eb2d5455705af3b
llegrand <biglouis06@hotmail.fr>
2019-02-05 12:16:15 +0100

    small

--- MCMC_SHMR_main_clean.py
+++ MCMC_SHMR_main_clean.py
@@ -47,7 +47,7 @@ import fitHMF_B18 as fitdespali16
 def load_smf(params):
     """Load the SMF."""
     smf_name = params['smf_name']
-    if smf_name == 'cosmos' or smf_name == 'cosmos_schechter':
+    if smf_name == 'cosmos' or smf_name == 'cosmos_schechter' or smf_name == 'cosmos_schechter_shifted':
 
         """Load the SMF from Iary Davidzon+17"""
         # redshifts of the Iari SMF
@@ -135,7 +135,7 @@ def load_smf(params):
                 smf[i][:, 2] = smf[i][:, 3] - smf[i][:, 1]
                 smf[i][:, 3] = temp
                 # Shift the smf values of + 1 sigma
-                smf[i][:, 1] += smf[0][:, 2]
+                smf[i][:, 1] += smf[i][:, 2]
 
             if params['do_sm_cut']:
                 params['SM_cut_min'] = np.log10(
@@ -395,7 +395,7 @@ def gauss(y, ksi):
 def phi_true(logMs, idx_z, M1, Ms0, beta, delta, gamma, ksi):
     """Use convolution defined in Behroozi et al 2010"""
     log_phidirect = log_phi_direct(logMs, idx_z, M1, Ms0, beta, delta, gamma)
-    if params['smf_name'] == 'cosmos_schechter':
+    if params['smf_name'] == 'cosmos_schechter' or params['smf_name'] == 'cosmos_schechter_shifted':
         dx = np.mean(logMs[1:] - logMs[:-1])
     else:
         print('Warning, the step between two mass bins in not defined in this case.')
